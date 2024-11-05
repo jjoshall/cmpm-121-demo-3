805c5bb166c3d4cf44787f5f2b0b4f090b419344
jowihall <148017202+jjoshall@users.noreply.github.com>
2024-11-05 12:07:39 -0800

    Added a button on screen that alerts a message

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-// todo
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+// Create a button element on the page that user can click to see a message
+const button = document.createElement("button");
+button.textContent = "Click me!";
+
+// change the text color of the button
+button.style.color = "red";
+
+document.body.appendChild(button);
+
+// Add a click event listener to the button
+button.addEventListener("click", () => {
+  alert("YOU CLICKED ME!");
+});
